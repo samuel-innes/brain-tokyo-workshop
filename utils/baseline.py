73f0314bf3bd7ed9fb589a54e55517e2f7162ab7
Samuel Innes <samuel@Samuels-MacBook-Air.local>
2023-12-23 13:34:10 +0000

    add code for classifier head baseline

--- utils/baseline.py
+++ utils/baseline.py
@@ -1,12 +1,18 @@
 from datasets import load_dataset, load_metric
-from transformers import BertTokenizer, BertForSequenceClassification, TrainingArguments, Trainer
+from transformers import BertTokenizer, BertModel, BertForSequenceClassification, TrainingArguments, Trainer
 import numpy as np
+import torch.nn as nn
 
 
 task = "cola"
 model_checkpoint = "bert-base-uncased"
 batch_size = 16
 num_epochs = 3
+
+# --- model params ----
+freeze_bert = True
+dense_layer = True
+# ---------------------
 
 
 dataset = load_dataset("glue", task)
@@ -25,9 +31,39 @@ print("Columns added by tokenizer:", tokenizer_columns)
 id2label = {0: "Invalid", 1: "Valid"}
 label2id = {val: key for key, val in id2label.items()}
 
-model = BertForSequenceClassification.from_pretrained(
-    model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id
-)
+class BERTDenseHeadModel(nn.Module):
+    def __init__(self):
+          super(BERTDenseHeadModel, self).__init__()
+          self.bert = BertModel.from_pretrained(model_checkpoint)
+          ### New layers:
+          self.linear1 = nn.Linear(768, 256)
+          self.linear2 = nn.Linear(256, 2) 
+
+    def forward(self, ids, mask):
+          sequence_output, pooled_output = self.bert(
+               ids, 
+               attention_mask=mask)
+
+          # sequence_output has the following shape: (batch_size, sequence_length, 768)
+          linear1_output = self.linear1(sequence_output[:,0,:].view(-1,768)) ## extract the 1st token's embeddings
+          linear2_output = self.linear2(linear1_output)
+
+          return linear2_output
+
+
+if not dense_layer:
+    model = BertForSequenceClassification.from_pretrained(
+        model_checkpoint, num_labels=2, id2label=id2label, label2id=label2id
+    )
+else:
+    model = BERTDenseHeadModel()
+
+#print(model)
+
+if freeze_bert:
+    for name, param in model.named_parameters():
+        if 'classifier' not in name: # classifier layer
+            param.requires_grad = False
 
 metric_name = "matthews_correlation"
 model_name = model_checkpoint.split("/")[-1]
